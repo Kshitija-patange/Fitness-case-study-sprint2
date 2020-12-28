562fbb1842b9a113fe5b547326bef15ae4c2c792
admin <admin@DESKTOP-2NPQ13G>
2020-12-28 09:17:26 +0530

    first commit

---
+++ fitness-case-study-sprint2/src/main/java/com/capgemini/fitnesscasestudysprint2/FitnessCaseStudySprint2Application.java
@@ -0,0 +1,13 @@
+package com.capgemini.fitnesscasestudysprint2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FitnessCaseStudySprint2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FitnessCaseStudySprint2Application.class, args);
+	}
+
+}
